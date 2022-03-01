94ad7162d57257cc84de874c81dd2af12b1b95ca
Petr_anisimov <68195712+PetrAnisimov@users.noreply.github.com>
2022-03-01 11:31:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import gspread
 import telebot
 
 
-bot_token = "968392706:AAFTwd6cS4H8Nyg3agr24V5OrqrZZBluuBY"
+bot_token = "muda968392706:AAFTwd6cS4H8Nyg3agr24V5OrqrZZBluuBYber"
 googlesheets_id = "1JLXI4Bt-I7BvPzKFtJq6PXIZSiMVxjKk536iQ4W8pu4"
 bot = telebot.TeleBot(bot_token)
 gc = gspread.service_account()
